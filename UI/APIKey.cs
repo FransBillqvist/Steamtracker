f16d09d4c8f8a82fb1323fae188384e705dd07f2
Frans Billquist <frans.billquist@gmail.com>
2021-12-06 14:29:09 +0100

    finding file for API

--- UI/APIKey.cs
+++ UI/APIKey.cs
@@ -3,7 +3,7 @@ using System.Net.Http;
 using System.Net;
 public class ApiKey
 {
-    private string key = System.IO.File.ReadAllText(@"./APIKEY.rtf");
+    private string key = System.IO.File.ReadAllText(@"../APIKEY.rtf");
 
     public ApiKey()
     {
--- UI/TempConsoleUI.cs
+++ UI/TempConsoleUI.cs
@@ -9,9 +9,9 @@ namespace Steamtracker
     public class TempConsoleUI
     {
         HttpClient client = new();
+        ApiKey apiKey = new();
         public async Task GetHttp()
         {
-            ApiKey apiKey = new();
             string newurl = apiKey.GetApiKey("http://api.steampowered.com/IPlayerService/GetRecentlyPlayedGames/v0001/?key=XXXXXXXXXXXXXXXXX&steamid=76561198150027303&format=json"); 
             string response = await client.GetStringAsync(newurl);
             Console.WriteLine(response);
